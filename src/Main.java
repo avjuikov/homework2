ed112d2891a589947a6c569e3ba37169d219e819
zhu4ara <a.v.juikov@gmail.com>
2024-03-31 22:30:13 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+int amount = 50;
+boolean isRegistered = true;
+int percent;
+
+if (isRegistered) {
+    percent = 3;
+} else {
+    percent = 1;
+}
+  //int percent = isRegistered ? 3 : 1;
+
+int bonus = amount / 100 * percent;
+if (bonus > 500) {
+    bonus = 500;
+}
+System.out.println("Итоговый бонус: " + bonus);
+}
+
+}
